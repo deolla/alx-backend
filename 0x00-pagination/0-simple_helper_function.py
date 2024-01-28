a0d2787665a4f62ba4fe784a8864614102a0e10e
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-28 11:09:45 +0100

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a function named index_range that takes two integer arguments page and page_size."""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing a start index and an end index."""
+    start_index = (page - 1) * page_size
+    end_index = page_size + start_index - 1
+    return (start_index, end_index)
