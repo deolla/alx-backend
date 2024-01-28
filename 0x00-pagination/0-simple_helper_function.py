01ea10e86d41b694269b6308997bef451a8bfb77
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-28 11:35:04 +0100

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Write a function named index_range that takes two integer arguments page and page_size."""
+"""Write a function named index_range that takes
+two integer arguments page and page_size."""
 from typing import Tuple
 
 
---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Write a function named index_range that takes
+two integer arguments page and page_size."""
+from typing import Tuple
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple containing a start index and an end index."""
+    start_index = (page - 1) * page_size
+    end_index = page_size + start_index
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page"""
+        assert (
+            isinstance(page, int) and page > 0
+        ), "page must be a number greater than 0"
+        assert (
+            isinstance(page_size, int) and page_size > 0
+        ), "page_size must be a number greater than 0"
+
+        dataset = self.dataset()
+        if dataset is None:
+            return []
+        start, end = index_range(page, page_size)
+        return dataset[start:end]
