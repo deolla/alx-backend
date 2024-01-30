de0a343fc413a5be54cfaf2f172889f4447facbd
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-30 13:08:11 +0100

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Create a class FIFOCache that inherits from BaseCaching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A caching system using FIFO eviction strategy"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item to the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Get the first item inserted (FIFO)
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD:", first_key)
+
+            # Add the new item
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve an item from the cache."""
+        if key is not None or key in self.cache_data:
+            return self.cache_data[key]
+        return None
