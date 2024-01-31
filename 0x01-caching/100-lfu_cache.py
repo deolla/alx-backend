fcb7790c4a506dc85a3c271346d34b18b3338c3a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-01-31 10:54:25 +0100

    caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,26 +10,21 @@ class LFUCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-        self.lfu_order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Add an item to the cache."""
-        if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                self.lfu_order.remove(key)
-            else:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    lfu = self.lfu_order.pop(0)
-                    del self.cache_data[lfu]
-                    print("DISCARD: {}".format(lfu))
-                self.cache_data[key] = item
-            self.lfu_order.append(key)
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = next(iter(self.cache_data))
+            print("DISCARD: {}".format(first))
+            self.cache_data.pop(first)
 
     def get(self, key):
         """Get an item from the cache."""
-        if key and key in self.cache_data:
-            self.lfu_order.remove(key)
-            self.lfu_order.append(key)
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
             return self.cache_data[key]
         return None
