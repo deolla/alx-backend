86579966c849d16ba7725a0c17319bda2eba1a20
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-06 21:07:50 +0100

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,7 +2,7 @@
 """A Basic Flask app that returns a simple HTML page."""
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="./templates")
 
 
 @app.route("/")
