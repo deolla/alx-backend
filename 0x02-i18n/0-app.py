41159a407d0dd018a744670ad42c83d36b8740e2
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-06 21:11:49 +0100

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,9 +8,7 @@ app = Flask(__name__, template_folder="./templates")
 @app.route("/")
 def index():
     """Index page of the app that returns a simple HTML page."""
-    return render_template(
-        "0-index.html", title="Welcome to Holberton", header="Hello World"
-    )
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":
