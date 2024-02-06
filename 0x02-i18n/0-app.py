75788df23853f76e4e9eaec69af8e982b4ae6ec7
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-02-06 21:02:22 +0100

    i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" Basic Flask app"""
+"""A Basic Flask app that returns a simple HTML page."""
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, __path__="/0x02-i18n")
 
 
 @app.route("/")
 def index():
-    """Index page"""
+    """Index page of the app that returns a simple HTML page."""
     return render_template(
         "0-index.html", title="Welcome to Holberton", header="Hello World"
     )
