442c283af0a78b70a8111c1da4e6d7b37aedd7f0
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-03-04 16:58:49 +0100

    que

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+// Create a queue with kue
+// Craete an Object containing the job data
+
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+    phoneNumber: 'string',
+    message: 'string'
+};
+
+// Create a job with the job data
+const job = queue.create('push_notification_code', jobData).save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+});
+
+// Listen for job completion
+job.on('complete', () => console.log('Notification job completed'));
+job.on('failed', () => console.log('Notification job failed'));
